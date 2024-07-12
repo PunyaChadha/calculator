3325eda1dabf8fdb76c05ee6222fad050d41a253
punyachadha <124513076+PunyaChadha@users.noreply.github.com>
2024-07-12 21:42:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,20 +14,6 @@ buttons.forEach((b) => {
 const displayValue = function(x){
     if(x=='equal'){
         val.style.color = 'rgb(255, 101, 255)';
-
-        // fetch('eval(result)')
-        //     .then((response) => {
-        //         val.innerHTML = eval(result);
-        //         answer.innerHTML = '';
-        //         result = eval(result);
-        //         clr = 1;
-        //     })
-        //     .catch((error) => {
-        //         val.innerHTML = 'Invalid Syntax';
-        //         answer.innerHTML = '';
-        //         result = '';
-        //     })
-
         try {
             val.innerHTML = String(eval(result));
             answer.innerHTML = '';
@@ -39,97 +25,101 @@ const displayValue = function(x){
             result = '';
         }
     }
-    else if(x=='C'){
-        val.innerHTML = '';
-        answer.innerHTML = '';
-        result = '';
-    }
-    else if(x=='delete'){
-        val.style.color = 'cyan';
-        clr = 0;
-        if(val.innerHTML == 'Invalid Syntax'){
-            val.innerHTML = '';
-        }
-        else{
-            val.innerHTML = val.innerHTML.slice(0, -1);
-            result = result.slice(0, -1);
-            
-            try {
-                answer.innerHTML = String(eval(result));
-            } catch (error) {
-                answer.innerHTML = '';
-            }
-            if(val.innerHTML == ''){
-                answer.innerHTML = '';
-            }
-            
-            // fetch('eval(result)')
-            //     .then((response) => {
-            //         answer.innerHTML = response;
-            //     })
-            //     .catch((error) => {
-            //         answer.innerHTML = '';
-            //     })
-            //     .finally(() => {
-            //         if(val.innerHTML == ''){
-            //             answer.innerHTML = '';
-            //         }
-            //     })
-        }
-    }
-    else if(x=='('){
-        val.style.color = 'cyan';
-        clr = 0;
-        val.innerHTML += 'x(';
-        result += '*(';
-        answer.innerHTML = String(eval(result));
-    }
-    else if(x=='negate'){
-        val.style.color = 'cyan';
-        clr = 0;
-        if(val.innerHTML.slice(-1)>=0 && val.innerHTML.slice(-1)<=9 && val.innerHTML != ''){
-            val.innerHTML += 'x(-';
-            result += '*(-';
-        }
-        else{
-            val.innerHTML += '(-';
-            result += '(-';
-        }
-        answer.innerHTML = String(eval(result));
-    }
     else{
-        val.style.color = 'cyan';
-        if(x>=0 && x<=9 && clr == 1){
+        if(x=='C'){
             val.innerHTML = '';
+            answer.innerHTML = '';
             result = '';
         }
-        clr = 0;
-        if(val.innerHTML == 'Invalid Syntax'){
-            val.innerHTML = '';
+        else if(x=='delete'){
+            val.style.color = 'cyan';
+            clr = 0;
+            if(val.innerHTML == 'Invalid Syntax'){
+                val.innerHTML = '';
+            }
+            else{
+                val.innerHTML = val.innerHTML.slice(0, -1);
+                result = result.slice(0, -1);
+            }
         }
-        val.innerHTML += x;
-        if(x=='x'){
-            x = '*';
+        else if(x=='('){
+            val.style.color = 'cyan';
+            clr = 0;
+            preVal = val.innerHTML.slice(-1);
+            if(preVal>=0 && preVal<=9 && val.innerHTML != ""){
+                val.innerHTML += 'x(';
+                result += '*(';
+            }
+            else{
+                val.innerHTML += '(';
+                result += '(';
+            }
         }
-        result += x;
-        answer.innerHTML = String(eval(result));
+        else if(x=='negate'){
+            val.style.color = 'cyan';
+            clr = 0;
+            preVal = val.innerHTML.slice(-1);
+            if(preVal>=0 && preVal<=9 && val.innerHTML != ""){
+                val.innerHTML += 'x(-';
+                result += '*(-';
+            }
+            else{
+                val.innerHTML += '(-';
+                result += '(-';
+            }
+        }
+        //to avoid repetition of operators
+        else if(x=='+' || x=='-' || x=='x' || x=='/'){
+            val.style.color = 'cyan';
+            clr = 0;
+            if(val.innerHTML == 'Invalid Syntax'){
+                val.innerHTML = '';
+            }
+            preVal = val.innerHTML.slice(-1);
+            if(preVal=='+' || preVal=='-' || preVal=='x' || preVal=='/'){
+                val.innerHTML = val.innerHTML.slice(0, -1);
+                result = result.slice(0, -1);   
+                val.innerHTML += x;
+                if(x=='x'){
+                    x = '*';
+                }
+                result += x;   
+            }
+            else{
+                val.innerHTML += x;
+                if(x=='x'){
+                    x = '*';
+                }
+                result += x;
+            }
+        }
+        else {
+            val.style.color = 'cyan';
+            if(x>=0 && x<=9 && clr == 1){
+                val.innerHTML = '';
+                result = '';
+            }
+            clr = 0;
+            if(val.innerHTML == 'Invalid Syntax'){
+                val.innerHTML = '';
+            }
+            val.innerHTML += x;
+            if(x=='x'){
+                x = '*';
+            }
+            result += x;
+        }
+    
+        //calculate the result
+        try {
+            answer.innerHTML = String(eval(result));
+        } catch (error) {
+            answer.innerHTML = '';
+        }
+        if(val.innerHTML == ''){
+            answer.innerHTML = '';
+        }
     }
-}
 
-// const blink = function(value){
-//     setTimeout( () => {
-//         value.innerHTML = '';
-//     },300);
-//     value.innerHTML = 'Invalid Syntax';
-//     setTimeout( () => {
-//         value.innerHTML = '';
-//     },300);
-//     value.innerHTML = 'Invalid Syntax';
-//     setTimeout( () => {
-//         value.innerHTML = '';
-//     },300);
-//     value.innerHTML = 'Invalid Syntax';
-//     setTimeout( () => {
-//         value.innerHTML = '';
-//     },300);
-// }
+    
+}
